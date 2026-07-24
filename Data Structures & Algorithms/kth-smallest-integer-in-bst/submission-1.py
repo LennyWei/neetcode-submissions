b3c09c7aaf5e275d17af237d62f43780453320ac
Kenny Liang <kpl5637@psu.edu>
2026-07-24 16:57:44 -0400

    Add: kth-smallest-integer-in-bst - submission-1

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-1.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        '''
+        dfs, as we visit nodes (starting from the bottom) add to a global counter?
+        would that gaurentee we explore nodes from smallest to largest?
+
+
+        '''
+        counter = 0
+        ret = -1
+
+        def dfs(root):
+            nonlocal counter
+            nonlocal ret
+
+            x = None
+
+            
+            if root.left:
+                dfs(root.left)
+            
+            counter += 1
+            print(f"added at {root.val}, counter is now {counter}")
+            if counter == k:
+                ret = root.val
+
+            
+            if root.right:
+                dfs(root.right)
+            
+
+            return None
+        
+        dfs(root)
+
+        return ret
