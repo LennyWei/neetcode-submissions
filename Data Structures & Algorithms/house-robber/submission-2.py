1c64ffe62a72f68bd81a41b8738749d50d3bfbde
Kenny Liang <kpl5637@psu.edu>
2026-08-31 19:44:06 -0400

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,43 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        '''
+        houses:  1 , 1 , 3 , 3
+
+        we need to find a permutation where max value, but no choices are adjacent
+        brute force, you can go through every permutation and choice 2^n time complexity  
+
+
+        kinda similar to previous problem, except max, and the choosing position is a bit different (we're choosing the two after the one after the house)
+        '''
+        # l = len(nums) # need to grab length before i extend
+
+        # nums += [0]
+
+        # for i in range(l-3, -1, -1):
+
+        #     nums[i] = max(nums[i+2], nums[i+3]) + nums[i]
+        
+        # print(nums)
+
+        # return max(nums[0], nums[1])
+
+        hashmap = {}
+
+        def dfs(i):
+
+            if i >= len(nums):
+                return 0
+            
+            if i in hashmap:
+                return hashmap[i]
+            
+            hashmap[i] = max(dfs(i+1), nums[i] + dfs(i+2))
+
+            return hashmap[i]
+        
+
+        return dfs(0)
+            # maintain the last two maxes that we can rob from at each index
+            
+
+
